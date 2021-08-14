1f6cec04ce64648c2b21f740d94e0a5a88e327e3
OphiliaFradarick <76613993+OphiliaFradarick@users.noreply.github.com>
2021-08-14 20:47:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var movingRect, fixedRect
+
+function setup(){
+  createCanvas(1600, 800);
+
+  fixedRect = createSprite(200,200,50,80);
+  fixedRect.shapeColor = "orange";
+  fixedRect.debug = true;
+
+  movingRect = createSprite(300,200,80,30);
+  movingRect.shapeColor = "orange";
+  movingRect.debug = true;
+
+}
+
+function draw(){
+  background(0);
+
+  movingRect.x = mouseX;
+  movingRect.y = mouseY;
+
+  if(movingRect.x - fixedRect.x < movingRect.width/2 + fixedRect.width/2
+    && fixedRect.x - movingRect.x < movingRect.width/2 + fixedRect.width/2){
+    movingRect.shapeColor="white";
+    fixedRect.shapeColor="white";
+  }
+  else{
+    movingRect.shapeColor="orange";
+    fixedRect.shapeColor="orange";
+  }
+
+  drawSprites();
+
+}
